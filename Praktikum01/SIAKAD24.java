20f39c89a3862e98ea97723bfae83b470fa5841d
septyandini921 <septyandini921@gmail.com>
2024-02-12 10:44:08 +0700

    Trial

---
+++ Praktikum01/SIAKAD24.java
@@ -0,0 +1,11 @@
+package Praktikum01;
+import java.util.Scanner;
+public class SIAKAD24 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Masukkan nilai angka untuk MK ");
+        
+    }
+}
